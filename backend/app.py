adcd9b1f447d0dec4a10e7636f449fb07fb5dbaa
keiakihito <kkatsumi.git@gmail.com>
2025-04-27 23:11:32 -0700

    Waveform

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,30 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=None)
 
 if os.getenv("FLASK_ENV") == "development":
     # Development
-    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    CORS(app, resources={r"/api/*": {"origins": "*"}, r"/static/*": {"origins": "*"}})
 else:
     # Deploy
     CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "https://your-frontend.vercel.app"}})
 
-AUDIO_FOLDER = "./audios"
+AUDIO_FOLDER = "audios"
 
+
+# User can see the list of audio file
 @app.route("/api/audios", methods=["GET"])
 def get_audio_list():
     files = [f for f in os.listdir(AUDIO_FOLDER)if f.endswith(".mp3")]
     return jsonify(files)
 
-# @app.route("/api/hello", methods=["GET"])
-# def hello():
-#     return {"message": "Hello from Flask!"}
+# User can see audio form
+@app.route("/static/<path:filename>", methods=["GET"])
+def serve_audio(filename):
+    return send_from_directory(AUDIO_FOLDER, filename)
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
--- frontend/src/ClipEditor.tsx
+++ frontend/src/ClipEditor.tsx
@@ -1,14 +1,38 @@
 // src/ClipEditor.tsx
 
+import { useEffect, useRef } from 'react'
 import { useParams } from 'react-router-dom'
+import WaveSurfer from 'wavesurfer.js'
 
 function ClipEditor() {
   const { audioName } = useParams<{ audioName: string }>()
+  const waveformRef = useRef<HTMLDivElement>(null)
+  const wavesurferRef = useRef<WaveSurfer | null>(null)
+
+  useEffect(() => {
+    if (waveformRef.current && audioName) {
+      const wavesurfer = WaveSurfer.create({
+        container: waveformRef.current,
+        waveColor: '#ddd',
+        progressColor: '#2196f3',
+        cursorColor: '#333',
+        height: 100,
+        url: `http://localhost:5003/static/${audioName}`, // Read mp3 file
+        backend: 'MediaElement',
+      })
+      wavesurferRef.current = wavesurfer
+    }
+
+    return () => {
+      wavesurferRef.current?.destroy()
+    }
+  }, [audioName])
 
   return (
     <div>
       <h1>Clip Editor</h1>
       <p>Selected Audio: {audioName}</p>
+      <div ref={waveformRef} />
     </div>
   )
 }
